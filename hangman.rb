a42a6dfa7fe435e669f4eb5a151c07f0c588d8e5
Parker <parkermatheson@gmail.com>
2018-04-06 15:05:03 -0600

    checks to see if a letter has already been used

--- hangman.rb
+++ hangman.rb
@@ -1,11 +1,29 @@
+@status = []
+
 def input
   letter = gets.strip.upcase
   if letter =~ /\A[A-Z]\z/
-    puts letter
+    if @status.include?(letter)
+      puts "that's already been used"
+      input
+    else
+      return letter
+    end
   else
     puts "Please use a single letter"
     input
   end
 end
-puts "puts a letter"
-input
+
+
+def disp_letters
+  p 
+end
+
+def letter_status input_status
+  @status << input_status
+end
+
+puts "please type a letter"
+letter_status input
+letter_status input
