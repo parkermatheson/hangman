818700abeedd88fc43fa0ff38ca43b0ce8ddc37f
Parker <parkermatheson@gmail.com>
2018-04-06 18:26:32 -0600

    begins creation of display

--- hangman.rb
+++ hangman.rb
@@ -1,29 +1,108 @@
-@status = []
+starting_sentence = "Our secret sentence"
+sentence_array = starting_sentence.split("").map(&:downcase)
+final_sentence = starting_sentence.gsub(/[a-zA-Z]/, "_").split("")
+not_dead = true
+unfinished = true
+mistakes = 0
 
-def input
-  letter = gets.strip.upcase
-  if letter =~ /\A[A-Z]\z/
-    if @status.include?(letter)
-      puts "that's already been used"
-      input
-    else
-      return letter
-    end
+puts "Welcome to hangman"
+while not_dead && unfinished
+  puts 'Please type a letter'
+  letter = gets.strip.downcase
+  if final_sentence.include?(letter)
+    puts "that's already been used"
   else
-    puts "Please use a single letter"
-    input
+    if sentence_array.include? letter
+      sentence_array.each_index { |i| final_sentence[i] = letter if sentence_array[i] == letter }
+    else 
+      puts "That letter is not in the sentence"
+      mistakes += 1
+    end
+    not_dead = false if mistakes >= 6
+    unfinished = false if final_sentence.join.casecmp(sentence_array.join).zero?
   end
+  hang_display mistakes
+  p final_sentence.join
+  puts "You lose" if !not_dead
+  puts "You win" if !unfinished
 end
 
-
-def disp_letters
-  p 
-end
-
-def letter_status input_status
-  @status << input_status
-end
-
-puts "please type a letter"
-letter_status input
-letter_status input
+def hang_display mistake
+  if mistake == 0
+    puts "           _______________ "
+    puts "          |               |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "      ____________________|"
+  elsif mistake == 1
+    puts "            ______________ "
+    puts "           |              |"
+    puts "          ----            |"
+    puts '        /      \          |'
+    puts "        |  . . |          |"
+    puts "        |   >  |          |"
+    puts '        \  --  /          |'
+    puts '          ----            |'
+    puts "          | |             |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "      ____________________|"
+  elsif mistake == 2
+    puts "            ______________ "
+    puts "           |              |"
+    puts "          ----            |"
+    puts '        /      \          |'
+    puts "        |  . . |          |"
+    puts "        |   >  |          |"
+    puts '        \  --  /          |'
+    puts '          ----            |'
+    puts "       ___|_|____         |"
+    puts "      |          |        |"
+    puts "      |          |        |"
+    puts "      |          |        |"
+    puts "      |          |        |"
+    puts "      |          |        |"
+    puts "      |          |        |"
+    puts "      |          |        |"
+    puts "      |__________|        |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "                          |"
+    puts "      ____________________|"
+  end
--- readme.md
+++ readme.md
@@ -1,8 +1,10 @@
 # hangman
 #   X input: single letters
-#   - word list (maybe predefined words list?)
+#   X word list (maybe predefined words list?)
 #   - display letters
 #   - display hanged man (double MVP)
 #   - store the status of letters
 #   - which letters have already been guessed
 #   - logic for letter input to word list
+#   - check to see if dead
+#   - check to see if finished
